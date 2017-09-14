6b1cb7f520df27fc9f2ec5c19a584795354920ea
Dilakshan <s.dilakshan@yahoo.com>
2017-09-14 18:02:16 +0530

    Update test.js

--- src/js/test.js
+++ src/js/test.js
@@ -1,23 +1,29 @@
-import {Join} from './join';
+import { Join } from './join';
 
- export default class Test extends Join{
+export default class Test extends Join {
 
-    constructor(name,id){
+    constructor(name, id) {
         super();
-    this.name = name;
-    this.id = id;
-    this.arr = [];
-}
+        this.name = name;
+        this.id = id;
+        this.arr = [];
+    }
 
-    myName(){
+    myName() {
         return super.alert() + this.name;
     }
-    myId(){
+    myId() {
         return this.id;
     }
-    returnArr(){
-        this.arr[0]=(this.name);
-        this.arr[1]=(this.id);
-      return this.arr;  
+    returnArr() {
+        this.arr[0] = (this.name);
+        this.arr[1] = (this.id);
+        return this.arr;
+    }
+    para() {
+        return super.alert();
+    }
+    static defaultPara() {
+        return new Test();
     }
 }
