8d9fd8c6cf780e44dd7544f990808916a272a991
Dilakshan <s.dilakshan@yahoo.com>
2017-09-14 18:01:07 +0530

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -4,12 +4,12 @@ import Join from './join';
 const title = "hello world";
 let count = [];
 
-let test = new Test("dilakshan","01515");
+let test = new Test("dilakshan", "01515");
 let array = [];
 array = test.returnArr();
 
-for(let i =0; i<array.length;i++){ 
-	count[i]=(array[i]);
+for (let i = 0; i < array.length; i++) {
+  count[i] = (array[i]);
 }
 
 
@@ -17,37 +17,44 @@ document.getElementById("title").innerHTML = title;
 document.getElementById("name").innerHTML = test.myName();
 document.getElementById("id").innerHTML = test.myId();
 document.getElementById("arr").innerHTML = `Your name is ${count[0]} and Your id is "${count[1]}"`;
-const url = "http://localhost:1337/user";
-fetch(url,{
-    method: 'get'
-})
-.then(data => data.json())
-.then(users => {
-	console.log(users);
-  let html = `
-   <thead>
-  <tr>
-  <th>ID</th>
-  <th>Name</th>
-  <th>&nbsp&nbsp&nbspCountry</th>
-  <th>Email</th>
-  </tr><br>
-</thead>`;
-  let count =0;
 
-  users.forEach(user => html += `
- 
-   <tbody>
-   <tr>
-   <td>${user.id}</td>
-   <td>&nbsp&nbsp&nbsp&nbsp&nbsp${user.name}</td>
-   <td>&nbsp&nbsp&nbsp${user.country}</td>
-   <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp${user.email}</td>
-   </tr>
-   </tbody>`);
-   
-  document.getElementById("user").innerHTML = html;
-  
-})
-.catch(e =>  console.log(e)
-);
+//static object
+var newTest = Test.defaultPara();
+
+//static test case
+// document.getElementById("test").innerHTML = newTest.para;
+
+// const url = "http://localhost:1337/user";
+// fetch(url,{
+//     method: 'get'
+// })
+// .then(data => data.json())
+// .then(users => {
+// 	console.log(users);
+//   let html = `
+//    <thead>
+//   <tr>
+//   <th>ID</th>
+//   <th>Name</th>
+//   <th>&nbsp&nbsp&nbspCountry</th>
+//   <th>Email</th>
+//   </tr><br>
+// </thead>`;
+//   let count =0;
+
+//   users.forEach(user => html += `
+
+//    <tbody>
+//    <tr>
+//    <td>${user.id}</td>
+//    <td>&nbsp&nbsp&nbsp&nbsp&nbsp${user.name}</td>
+//    <td>&nbsp&nbsp&nbsp${user.country}</td>
+//    <td>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp${user.email}</td>
+//    </tr>
+//    </tbody>`);
+
+//   document.getElementById("user").innerHTML = html;
+
+// })
+// .catch(e =>  console.log(e)
+// );
